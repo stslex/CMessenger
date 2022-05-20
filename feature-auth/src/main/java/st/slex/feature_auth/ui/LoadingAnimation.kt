2e00e918a27df821976913fa3cdaa6362295f97f
stslex <ilya977.077@gmail.com>
2022-05-20 19:38:58 +0300

    init loading screen

--- feature-auth/src/main/java/st/slex/feature_auth/ui/LoadingAnimation.kt
+++ feature-auth/src/main/java/st/slex/feature_auth/ui/LoadingAnimation.kt
@@ -1,76 +1,88 @@
 package st.slex.feature_auth.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import st.slex.core_ui.theme.CMessengerTheme
-import kotlin.math.absoluteValue
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 @Composable
 fun Loading(
     modifier: Modifier = Modifier
 ) {
-    val density = LocalDensity.current.density
-    val configuration = LocalConfiguration.current
-    val smallestScreenWidth = configuration.smallestScreenWidthDp * density
-    val circleRadius = 20f
-    val primaryAnchor = smallestScreenWidth / 2
-    val stateX = remember { mutableStateOf(primaryAnchor) }
-    val stateY = remember { mutableStateOf(primaryAnchor - primaryAnchor / 2) }
-    val step = primaryAnchor / 20
+    for (i in 0..5) {
+        LoadingItem(modifier = modifier, initialState = i * 60.0)
+    }
+}
 
+@Composable
+fun LoadingItem(
+    modifier: Modifier = Modifier, initialState: Double
+) {
+    var radian by remember { mutableStateOf(initialState) }
+    val circleRadius = 30.0 * abs(cos(radian))
+    val collapseRadius = 200.0 * abs(cos(radian))
+    val circleColor = MaterialTheme.colorScheme.primary
+    Canvas(modifier = modifier, onDraw = {
+        val centerX = (collapseRadius * cos(radian)).toFloat()
+        val centerY = (collapseRadius * sin(radian)).toFloat()
+        val circleCenter = Offset(centerX, centerY)
+        drawCircle(
+            color = circleColor,
+            radius = circleRadius.toFloat(),
+            center = circleCenter,
+            alpha = abs(0.7f * cos(radian).toFloat())
+        )
+    })
     LaunchedEffect(key1 = true) {
         while (true) {
-            if (stateX.value >= primaryAnchor && stateY.value <= primaryAnchor) {
-                stateX.value = stateX.value + step
-                stateY.value = stateY.value + step
-            }
-            if (stateX.value >= primaryAnchor && stateY.value >= primaryAnchor) {
-                stateX.value = stateX.value - step
-                stateY.value = stateY.value + step
-            }
-            if (stateX.value <= primaryAnchor && stateY.value >= primaryAnchor) {
-                stateX.value = stateX.value - step
-                stateY.value = stateY.value - step
-            }
-            if (stateX.value <= primaryAnchor && stateY.value <= primaryAnchor) {
-                stateX.value = stateX.value + step
-                stateY.value = stateY.value - step
-            }
+            radian += 0.008
+            radian %= 360
             delay(1)
         }
-
     }
-
-    Canvas(
-        modifier = modifier.size(smallestScreenWidth.dp),
-        onDraw = {
-            drawCircle(
-                Brush.linearGradient(
-                    colors = listOf(Color.Magenta, Color.Cyan)
-                ),
-                radius = circleRadius,
-                center = Offset(stateX.value, stateY.value)
-            )
-        })
 }
 
 @Preview(
-    group = "tools", name = "Primary Loading", device = "id:pixel_4", showBackground = true,
-    showSystemUi = true
+    group = "tools",
+    name = "Primary Loading",
+    device = "id:pixel_4",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
 fun LoadingPreview() {
-    CMessengerTheme() {
-        Loading()
+    CMessengerTheme(dynamicColor = true) {
+        Box(
+            Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxSize()
+        ) {
+            LazyColumn {
+                items(100) {
+                    Button(modifier = Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth(), onClick = { }) {}
+                }
+            }
+            Loading(Modifier.align(Alignment.Center))
+        }
     }
 }
